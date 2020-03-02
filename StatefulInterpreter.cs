018623e9182282b2f4c013c4989b1f8eeb52022c
DRed <dima.redkin.1999@gmail.com>
2020-03-02 20:31:42 +0500

    added class ConsoleWithStorage. Add, median and random works. need to refactor random

--- StatefulInterpreter.cs
+++ StatefulInterpreter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Reflection.Emit;
 using static System.Text.Encoding;
 
 namespace CommandLineCalculator
@@ -10,27 +11,18 @@ namespace CommandLineCalculator
     {
         private static CultureInfo Culture => CultureInfo.InvariantCulture;
 
-        private UserConsole _userConsole;
-
-        private Storage _storage;
-        private byte[] StorageBytes => _storage.Read();
-
-        private IEnumerable<string> StorageLines => UTF8.GetString(StorageBytes).Trim()
-            .Split('\n').Where(line => !string.IsNullOrEmpty(line));
-
-        private long NextRandomValue => System.Convert.ToInt64(StorageLines.First());
-        private List<string> CurrentCommandLines => StorageLines.Skip(1).ToList();
+        private ConsoleWithStorage _consoleWithStorage;
 
         public override void Run(UserConsole userConsole, Storage storage)
         {
-            _storage = storage;
-            _userConsole = userConsole;
+            _consoleWithStorage = new ConsoleWithStorage(userConsole, storage);
 
-            InitializeStorageIfEmpty();
+            // var x = System.Convert.ToInt64(_consoleWithStorage.ReadLine());
+
 
             while (true)
             {
-                var commandName = ReadCurrentCommandName(CurrentCommandLines);
+                var commandName = _consoleWithStorage.ReadLine();
 
                 switch (commandName)
                 {
@@ -43,142 +35,46 @@ namespace CommandLineCalculator
                         Median();
                         break;
                     case "rand":
-                        Random();
+                        var x =
+                            Random();
+                        _consoleWithStorage.ChangeCurrentRandomValueFor(x);
                         break;
-                    case "help":
-                        Help();
-                        break;
+                    // case "help":
+                    //     Help();
+                    //     break;
                     default:
-                        _userConsole.WriteLine("Такой команды нет, используйте help для списка команд");
+                        _consoleWithStorage.WriteLine("Такой команды нет, используйте help для списка команд");
                         break;
                 }
 
-                ClearStorageAndWriteNextRandom();
-            }
-
-
-            void InitializeStorageIfEmpty()
-            {
-                if (StorageBytes == null || StorageBytes.Length == 0)
-                {
-                    const long firstRandomValue = 420L;
-                    AddToStorage(firstRandomValue);
-                }
+                _consoleWithStorage.CommandIsDone();
             }
         }
 
-        private string ReadCurrentCommandName(List<string> storageLines)
-        {
-            string commandName;
-            if (storageLines.Any())
-            {
-                commandName = storageLines.First();
-            }
-            else
-            {
-                commandName = _userConsole.ReadLine().Trim();
-                AddToStorage(commandName);
-            }
-
-            return commandName;
-        }
 
         private void Add()
         {
-            const int totalCountOfArguments = 2;
-            var argumentsFromStorage = CurrentCommandLines.Skip(1).ToList();
-            var remainingCountOfArguments = totalCountOfArguments - argumentsFromStorage.Count;
-
-            ReadFromConsoleAndAddToStorageFor(remainingCountOfArguments);
-
-            var argumentsToExecuteAdd = CurrentCommandLines.Skip(1).ToList();
-            var numbersToExecuteAdd = argumentsToExecuteAdd.ConvertAll(int.Parse);
-            _userConsole.WriteLine(numbersToExecuteAdd.Sum().ToString(Culture));
+            var a = ReadNumberFromConsole();
+            var b = ReadNumberFromConsole();
+            _consoleWithStorage.WriteLine((a + b).ToString(Culture));
         }
 
-        private void ReadFromConsoleAndAddToStorageFor(int count)
+        private int ReadNumberFromConsole()
         {
-            for (var i = 0; i < count; i++)
-            {
-                var value = ReadNumberFromConsole();
-                AddToStorage(value);
-            }
+            return int.Parse(_consoleWithStorage.ReadLine().Trim(), Culture);
         }
 
         private void Median()
         {
-            var argumentsFromStorage = CurrentCommandLines.Skip(1).ToList();
-            var totalCountOfArguments = ReadTotalCountOfArguments(argumentsFromStorage);
-            argumentsFromStorage = argumentsFromStorage.Skip(1).ToList();
-            var remainingCountOfArguments = totalCountOfArguments - argumentsFromStorage.Count;
-
-            ReadFromConsoleAndAddToStorageFor(remainingCountOfArguments);
-
-            var argumentsToExecuteMedian = CurrentCommandLines.Skip(2).ToList();
-            var numbersToExecuteMedian = argumentsToExecuteMedian.ConvertAll(int.Parse);
-            _userConsole.WriteLine(CalculateMedian(numbersToExecuteMedian).ToString(Culture));
-        }
-
-        private int ReadTotalCountOfArguments(List<string> argumentsAtStart)
-        {
-            int totalCountOfArguments;
-
-            if (argumentsAtStart.Count == 0)
+            var count = ReadNumberFromConsole();
+            var numbers = new List<int>();
+            for (var i = 0; i < count; i++)
             {
-                totalCountOfArguments = ReadNumberFromConsole();
-                AddToStorage(totalCountOfArguments);
-            }
-            else
-            {
-                totalCountOfArguments = int.Parse(argumentsAtStart.First());
+                numbers.Add(ReadNumberFromConsole());
             }
 
-            return totalCountOfArguments;
-        }
-
-        private void Random()
-        {
-            const int a = 16807;
-            const int m = 2147483647;
-
-            var argumentsFromStorage = CurrentCommandLines.Skip(1).ToList();
-            var totalCountOfArguments = ReadTotalCountOfArguments(argumentsFromStorage);
-            argumentsFromStorage = argumentsFromStorage.Skip(1).ToList();
-            var remainingCountOfArguments = totalCountOfArguments - argumentsFromStorage.Count;
-
-            for (var i = 0; i < remainingCountOfArguments; i++)
-            {
-                var currentRandomValue = NextRandomValue;
-                _userConsole.WriteLine(currentRandomValue.ToString(Culture));
-                AddToStorage(currentRandomValue);
-                
-                ChangeCurrentRandomValueFor(a * currentRandomValue % m);
-            }
-        }
-
-        private void ChangeCurrentRandomValueFor(long nextRandomValue)
-        {
-            var randomValueBytes = UTF8.GetBytes($"{nextRandomValue}\n");
-            var currentCommandBytes = UTF8.GetBytes(string.Join("\n", CurrentCommandLines) + "\n");
-            _storage.Write(randomValueBytes.Concat(currentCommandBytes).ToArray());
-        }
-
-        private void ClearStorageAndWriteNextRandom()
-        {
-            var nextRandomValue = NextRandomValue;
-            _storage.Write(Array.Empty<byte>());
-            _storage.Write(UTF8.GetBytes($"{nextRandomValue}\n"));
-        }
-
-        private void AddToStorage(string value)
-        {
-            var valueBytes = UTF8.GetBytes($"{value}\n");
-            _storage.Write(StorageBytes.Concat(valueBytes).ToArray());
-        }
-
-        private void AddToStorage(long value)
-        {
-            AddToStorage(value.ToString(Culture));
+            var result = CalculateMedian(numbers);
+            _consoleWithStorage.WriteLine(result.ToString(Culture));
         }
 
         private double CalculateMedian(List<int> numbers)
@@ -194,63 +90,203 @@ namespace CommandLineCalculator
             return (numbers[count / 2 - 1] + numbers[count / 2]) / 2.0;
         }
 
-        private void Help()
+
+        private long Random()
         {
-            const string exitMessage = "Чтобы выйти из режима помощи введите end";
-            const string commands = "Доступные команды: add, median, rand";
+            const int a = 16807;
+            const int m = 2147483647;
+            var x = _consoleWithStorage.GetNextRandom();
 
-            var helpLinesFromStorage = CurrentCommandLines.Skip(1).ToList();
-
-            WriteMessageIfWasNotWritten("Укажите команду, для которой хотите посмотреть помощь");
-            WriteMessageIfWasNotWritten(commands);
-            WriteMessageIfWasNotWritten(exitMessage);
-
-            while (true)
+            var count = ReadNumberFromConsole();
+            for (var i = 0; i < count; i++)
             {
-                var commandName = ReadCurrentCommandName(helpLinesFromStorage);
-                helpLinesFromStorage = helpLinesFromStorage.Skip(1).ToList();
-
-                switch (commandName)
-                {
-                    case "end":
-                        return;
-                    case "add":
-                        WriteMessageIfWasNotWritten("Вычисляет сумму двух чисел");
-                        WriteMessageIfWasNotWritten(exitMessage);
-                        break;
-                    case "median":
-                        WriteMessageIfWasNotWritten("Вычисляет медиану списка чисел");
-                        WriteMessageIfWasNotWritten(exitMessage);
-                        break;
-                    case "rand":
-                        WriteMessageIfWasNotWritten("Генерирует список случайных чисел");
-                        WriteMessageIfWasNotWritten(exitMessage);
-                        break;
-                    default:
-                        WriteMessageIfWasNotWritten("Такой команды нет");
-                        WriteMessageIfWasNotWritten(commands);
-                        WriteMessageIfWasNotWritten(exitMessage);
-                        break;
-                }
+                _consoleWithStorage.WriteLine(x.ToString(Culture));
+                x = a * x % m;
             }
 
-            void WriteMessageIfWasNotWritten(string message)
+            return x;
+        }
+
+
+        #region Help
+
+        // private void Help()
+        // {
+        //     const string exitMessage = "Чтобы выйти из режима помощи введите end";
+        //     const string commands = "Доступные команды: add, median, rand";
+        //
+        //     var helpLinesFromStorage = CurrentCommandLines.Skip(1).ToList();
+        //
+        //     WriteMessageIfWasNotWritten("Укажите команду, для которой хотите посмотреть помощь");
+        //     WriteMessageIfWasNotWritten(commands);
+        //     WriteMessageIfWasNotWritten(exitMessage);
+        //
+        //     while (true)
+        //     {
+        //         var commandName = ReadCurrentCommandName(helpLinesFromStorage);
+        //         helpLinesFromStorage = helpLinesFromStorage.Skip(1).ToList();
+        //
+        //         switch (commandName)
+        //         {
+        //             case "end":
+        //                 return;
+        //             case "add":
+        //                 WriteMessageIfWasNotWritten("Вычисляет сумму двух чисел");
+        //                 WriteMessageIfWasNotWritten(exitMessage);
+        //                 break;
+        //             case "median":
+        //                 WriteMessageIfWasNotWritten("Вычисляет медиану списка чисел");
+        //                 WriteMessageIfWasNotWritten(exitMessage);
+        //                 break;
+        //             case "rand":
+        //                 WriteMessageIfWasNotWritten("Генерирует список случайных чисел");
+        //                 WriteMessageIfWasNotWritten(exitMessage);
+        //                 break;
+        //             default:
+        //                 WriteMessageIfWasNotWritten("Такой команды нет");
+        //                 WriteMessageIfWasNotWritten(commands);
+        //                 WriteMessageIfWasNotWritten(exitMessage);
+        //                 break;
+        //         }
+        //     }
+        //
+        //     void WriteMessageIfWasNotWritten(string message)
+        //     {
+        //         if (helpLinesFromStorage.Count != 0)
+        //         {
+        //             helpLinesFromStorage = helpLinesFromStorage.Skip(1).ToList();
+        //         }
+        //         else
+        //         {
+        //             _userConsole.WriteLine(message);
+        //             AddToStorage(message);
+        //         }
+        //     }
+        // }
+
+        #endregion
+
+
+        private class ConsoleWithStorage : UserConsole
+        {
+            private static CultureInfo Culture => CultureInfo.InvariantCulture;
+
+            private UserConsole _console;
+            private Storage _storage;
+
+            private int _currentLine = 1;
+            private byte[] StorageBytes => _storage.Read();
+
+            private List<string> StorageLines => UTF8.GetString(StorageBytes).Trim()
+                .Split('\n').Where(line => !string.IsNullOrEmpty(line)).ToList();
+
+            // private long NextRandomValue => System.Convert.ToInt64(StorageLines.First());
+            // private List<string> CurrentCommandLines => StorageLines.Skip(1).ToList();
+
+            public ConsoleWithStorage(UserConsole console, Storage storage)
             {
-                if (helpLinesFromStorage.Count != 0)
+                _console = console;
+                _storage = storage;
+
+                // _currentLine = StorageLines.Count();
+                InitializeStorageIfEmpty();
+            }
+
+            private void InitializeStorageIfEmpty()
+            {
+                if (StorageBytes == null || StorageBytes.Length == 0)
                 {
-                    helpLinesFromStorage = helpLinesFromStorage.Skip(1).ToList();
+                    const long firstRandomValue = 420L;
+                    AddToStorage(firstRandomValue);
+                }
+
+                // else
+                // {
+                //     _currentLine = StorageLines.Count();
+                // }
+            }
+
+            private void AddToStorage(string value)
+            {
+                var valueBytes = UTF8.GetBytes($"{value}\n");
+                _storage.Write(StorageBytes.Concat(valueBytes).ToArray());
+            }
+
+            private void AddToStorage(long value)
+            {
+                AddToStorage(value.ToString(Culture));
+            }
+
+            public override string ReadLine()
+            {
+                string readLine;
+                if (_currentLine < StorageLines.Count())
+                {
+                    readLine = StorageLines[_currentLine];
                 }
                 else
                 {
-                    _userConsole.WriteLine(message);
-                    AddToStorage(message);
+                    readLine = _console.ReadLine().Trim();
+                    AddToStorage(readLine);
                 }
-            }
-        }
 
-        private int ReadNumberFromConsole()
-        {
-            return int.Parse(_userConsole.ReadLine().Trim(), Culture);
+                _currentLine++;
+
+                return readLine;
+            }
+
+            public void ChangeCurrentRandomValueFor(long nextRandomValue)
+            {
+                var randomValueBytes = UTF8.GetBytes($"{nextRandomValue}\n");
+                var currentCommandBytes = UTF8.GetBytes(string.Join("\n", StorageLines.Skip(1)) + "\n");
+                _storage.Write(randomValueBytes.Concat(currentCommandBytes).ToArray());
+            }
+
+            // public string ReadCurrentCommandName(List<string> storageLines)
+            // {
+            //     string commandName;
+            //     if (storageLines.Any())
+            //     {
+            //         commandName = storageLines.First();
+            //     }
+            //     else
+            //     {
+            //         commandName = _console.ReadLine().Trim();
+            //         AddToStorage(commandName);
+            //     }
+            //
+            //     return commandName;
+            // }
+
+            public override void WriteLine(string content)
+            {
+                //если в текущей строке ничего нет, то пишем, иначе не пишем
+                if (_currentLine >= StorageLines.Count())
+                {
+                    _console.WriteLine(content);
+                    AddToStorage(content);
+                }
+
+                _currentLine++;
+            }
+
+            private void ClearStorageAndWriteNextRandom()
+            {
+                var nextRandomValue = StorageLines[0];
+                _storage.Write(Array.Empty<byte>());
+                _storage.Write(UTF8.GetBytes($"{nextRandomValue}\n"));
+            }
+
+            public void CommandIsDone()
+            {
+                _currentLine = 1;
+                ClearStorageAndWriteNextRandom();
+            }
+
+            public long GetNextRandom()
+            {
+                return System.Convert.ToInt64(StorageLines[0]);
+            }
         }
     }
 }
